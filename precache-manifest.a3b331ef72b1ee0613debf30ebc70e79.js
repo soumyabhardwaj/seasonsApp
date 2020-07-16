a6249784a33da09f081e15737b0dc3c06adfed7a
soumya bhardwaj <bsoumya175@gmail.com>
2020-07-16 18:41:14 +0530

    Updates

---
+++ precache-manifest.a3b331ef72b1ee0613debf30ebc70e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3697e650d48f2b61b8cb80739c378599",
+    "url": "/soumyabhardwaj/seasonsApp.git/index.html"
+  },
+  {
+    "revision": "41a6921d1c3f36367af1",
+    "url": "/soumyabhardwaj/seasonsApp.git/static/css/main.9489452e.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/soumyabhardwaj/seasonsApp.git/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/soumyabhardwaj/seasonsApp.git/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a6921d1c3f36367af1",
+    "url": "/soumyabhardwaj/seasonsApp.git/static/js/main.b8cb1fed.chunk.js"
+  },
+  {
+    "revision": "3b41ecce6da7603f44bf",
+    "url": "/soumyabhardwaj/seasonsApp.git/static/js/runtime-main.d59de8f7.js"
+  }
+]);
